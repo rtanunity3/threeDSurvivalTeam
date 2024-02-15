a5bd724d14032098729f067b19705f09edb91bc8
oeto2 <oeto2@naver.com>
2024-02-15 15:36:24 +0900

    Feat 프롤로그 작업 완료

--- Assets/Scripts/Managers/PrologueManager.cs
+++ Assets/Scripts/Managers/PrologueManager.cs
@@ -8,11 +8,21 @@ public class PrologueManager : MonoBehaviour
     AudioSource audioSource;
     [SerializeField] AudioClip car_Claxon;
 
+    private Animator animator;
+
     public void Awake()
     {
         audioSource = GetComponent<AudioSource>();
+        animator = GetComponent<Animator>();
     }
 
     public void PlayCarClaxon() => audioSource.PlayOneShot(car_Claxon);
     public void LoadMainScene() => SceneManager.LoadScene("MainScene");
+
+    public void StartButtonClick() => animator.SetTrigger("StartPrologue");
+    public void EndButtonClick()
+    {
+        Debug.Log("게임종료");
+        Application.Quit();
+    }
 }
